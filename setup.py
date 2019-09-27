9f1ba3352a8c0faffd59f425b813268f62e5828e
Mike D <mongoos2006@users.noreply.github.com>
2019-09-27 13:09:41 -0400

    typo in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('README.md') as f:
     readme = f.read()
 
 setup(name='python_tree',
-      url='https://github.com/mongoos2006/python-unix-command-imp',
+      url='https://github.com/mongoos2006/python_tree',
       author="Mike D",
       packages=find_packages(),
       install_requires=[
